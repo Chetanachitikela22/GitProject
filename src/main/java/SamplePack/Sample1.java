2a06e30b66ec7918215041ffd9a3f9016a71cf06
Mittu22 <chetanachitikela22@gmail.com>
2023-01-21 15:50:33 +0530

    Add files via upload

---
+++ src/main/java/SamplePack/Sample1.java
@@ -0,0 +1,10 @@
+package SamplePack;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+       @Test
+       public void testCase1() {
+    	   System.out.println("This is new script fot committing");
+       }
+}
